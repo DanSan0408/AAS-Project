76826db216dc6f781eddf596c434311ef2e2de4e
Danial Ihsan Bin Mohd Nadhir <ihsan.rubix@gmail.com>
2026-08-18 14:12:06 +0800

    data group student fix

--- adproject/src/main/java/com/capstone/adproject/model/Group.java
+++ adproject/src/main/java/com/capstone/adproject/model/Group.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.Filter;
 import org.hibernate.annotations.FilterDef;
 import org.hibernate.annotations.ParamDef;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -32,7 +33,10 @@ public class Group {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "group_name")
     private String groupName;
+    
+    @Column(name = "group_size")
     private int groupSize;
 
     @ManyToOne
